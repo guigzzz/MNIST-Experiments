0cc1c6b145970b5a2eadd412742639ecf639719d
manip <manip>
2016-08-29 16:25:09 +0200

    got it working

--- net_wrapper.py
+++ net_wrapper.py
@@ -1,27 +1,27 @@
 import network
 import numpy as np
 
+ #network learns to map 0.05 to 0.01 and 0.1 to 0.99
+
 training_data = [0.05,0.1]
 
 checking_data = [0.01,0.99]
 
 layers = [2, 2, 2]
 
-epoch = 10000
+epochs = 10000
 
 learning_rate = 0.5
 
 biases = [0.35, 0.6]
 
-weights = [0.15, 0.25, 0.2, 0.3, 0.4, 0.45, 0.5, 0.55]
+weights = [[0.15, 0.2, 0.25, 0.3], [0.4, 0.45, 0.5, 0.55]]
 
 #initialise network
 
-net = network.network(training_data, checking_data, layers, epoch,
+net = network.network(training_data, checking_data, layers, epochs,
                       learning_rate, biases, weights)
 
+#run network
+
 net.run()
-
-
-
-
--- network.py
+++ network.py
@@ -10,7 +10,7 @@ class network(object):
         self.layers = layers
         self.epochs = epochs
         self.learning_rate = learning_rate
-        self.biases = biases
+        self.biases = biases #add random generation
         self.weights = weights
         self.all_outputs = []
 
@@ -22,7 +22,7 @@ class network(object):
         if layer_count != len(self.layers): #check for base case, e.g when calculated output values
             outputs = []
             for neuron in range(self.layers[layer_count]):
-                output = sigmoid(sum(data[i]*self.weights[(layer_count-1)*4 + 2*neuron +i] for i in range(self.layers[layer_count-1])) + self.biases[layer_count-1])
+                output = sigmoid(sum(data[i]*self.weights[(layer_count-1)][2*neuron +i] for i in range(self.layers[layer_count-1])) + self.biases[layer_count-1])
                 outputs.append(output)
 
             self.all_outputs.append(outputs)
@@ -31,36 +31,104 @@ class network(object):
         return data
 
 
-    def backprop(self): # dE/dw = -(target(o1)-out(o1)) * out(o1)*(1-out(o1))*out(h1)
+    def backprop(self): # dE/dw = -(target(o1)-out(o1)) * out(o1)*(1-out(o1))*out(h1) , out(o1)*(1-out(o1)) = sigmoid_prime(out(o1))
+
         tmp_weights = []
+        self.all_outputs = [self.training_data] + self.all_outputs
 
-        """
-        for weight in reversed(self.weights):
-            
 
-            new_weight = weight - self.learning_rate * dEdw
-            tmp_weights.append(new_weight)
 
-        return tmp_weights
-        """
+        #do network output layer
+
+
+
+
+        delta = []
+        deltah = []
+
+        for i in range(self.layers[-1]):
+            delta.append(-(self.checking_data[i]-self.all_outputs[-1][i]) * sigmoid_prime(self.all_outputs[-1][i]))
+        
+
+        neuron_count_previous = self.layers[-2]
+
+
+
+        for i in range(len(self.weights[-1])/2):
+            discard_delta = delta[i]
+            for j in range(neuron_count_previous):
+                tmp_weights.append(self.weights[-1][2*i+j] - self.learning_rate*discard_delta*self.all_outputs[-2][j])
+
+
+
+
+        #do the rest
+
+
+
+
+        for i in range(self.layers[-2]):
+
+            delta_tmp = sum(delta[k]*self.weights[-1][k] for k in range(self.layers[-2]))
+
+            deltah.append(delta_tmp*sigmoid_prime(self.all_outputs[-2][i])) #check sum
+
+
+        for i in range(len(self.weights[-2])/2):
+            discard_delta = deltah[i]
+            for j in range(neuron_count_previous): 
+                tmp_weights.append(self.weights[-2][2*i+j] - self.learning_rate*discard_delta*self.all_outputs[-3][j])
+
+
+
+
+
+        chunksize = 4
+        self.weights = list(reversed([tmp_weights[i:i + chunksize] for i in xrange(0, len(tmp_weights), chunksize)]))
 
 
 
     
     def run(self):
         error = 0
-        for i in range(1): #, self.epochs
 
-
+        for i in range(1, self.epochs+1): #, self.epochs
             outputs = self.feedforward(self.training_data, 1)
             #print(outputs)
             error = sum(0.5*np.square(self.checking_data[i]-outputs[i]) for i in range(len(outputs)))
-            self.weights = self.backprop()
-            print('epoch: ' + str(i+1) + ', error: ' + str(error))
-            print(self.all_outputs)
 
-            self.all_outputs = []
-    
+            """
+
+            print('initial weights')
+            print(self.weights)
+            print('')
+
+            """
+
+            self.backprop()
+
+            if i % 10000 == 0:
+                print(('epoch: ' + str(i) + ', MSE: ' + str(error)) )
+
+            """
+
+            print('')
+            print('all_outputs')
+            print(self.all_outputs)
+            print('')
+            print('weights')
+            print(self.weights)
+            print('')
+
+            """
+
+            #print([self.training_data] + self.all_outputs)
+
+            self.all_outputs = [] #reset outputs
+
+        print('outputs at final epoch:')
+        print('')
+        print(outputs)
 
 
 def sigmoid(z):
