03cc15c24f2d9afb67edda97cf64611340c0efcc
Guillaume <rameg@neuf.fr>
2016-08-30 10:23:26 +0200

    update

--- net_wrapper.py
+++ net_wrapper.py
@@ -1,11 +1,11 @@
 import network
-import numpy as np
+# import numpy as np
 
- #network learns to map 0.05 to 0.01 and 0.1 to 0.99
+# network learns to map 0.05 to 0.01 and 0.1 to 0.99
 
-training_data = [0.05,0.1]
+training_data = [0.05, 0.1]
 
-checking_data = [0.01,0.99]
+checking_data = [0.01, 0.99]
 
 layers = [2, 2, 2]
 
@@ -17,11 +17,11 @@ biases = [0.35, 0.6]
 
 weights = [[0.15, 0.2, 0.25, 0.3], [0.4, 0.45, 0.5, 0.55]]
 
-#initialise network
+# initialise network
 
 net = network.network(training_data, checking_data, layers, epochs,
                       learning_rate, biases, weights)
 
-#run network
+# run network
 
 net.run()
--- network.py
+++ network.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 
+
 class network(object):
 
     def __init__(self, training_data, checking_data, layers, epochs, learning_rate, biases, weights):
@@ -9,8 +10,8 @@ class network(object):
         self.layers = layers
         self.epochs = epochs
         self.learning_rate = learning_rate
-        self.biases = biases 
-        self.weights = weights #add random generation
+        self.biases = biases
+        self.weights = weights  # add random generation
         """
         self.biases = [np.random.randn(y, 1) for y in layers[1:]]
         self.weights = [np.random.randn(y, x)
@@ -19,87 +20,75 @@ class network(object):
         self.all_outputs = []
 
     def feedforward(self, data, layer_count):
-        
+
         if layer_count == 1:
             data = self.training_data
 
-        if layer_count != len(self.layers): #check for base case, e.g when calculated output values
+        # check for base case, e.g when calculated output values
+        if layer_count != len(self.layers):
             outputs = []
             for neuron in range(self.layers[layer_count]):
-                output = sigmoid(sum(data[i]*self.weights[(layer_count-1)][2*neuron +i] for i in range(self.layers[layer_count-1])) + self.biases[layer_count-1])
+                output = sigmoid(sum(data[i] * self.weights[(layer_count - 1)][2 * neuron + i]
+                                     for i in range(self.layers[layer_count - 1])) + self.biases[layer_count - 1])
                 outputs.append(output)
 
             self.all_outputs.append(outputs)
-            return self.feedforward(outputs,layer_count+1)
+            return self.feedforward(outputs, layer_count + 1)
 
         return data
 
-
-    def backprop(self): # dE/dw = -(target(o1)-out(o1)) * out(o1)*(1-out(o1))*out(h1) , out(o1)*(1-out(o1)) = sigmoid_prime(out(o1))
+    def backprop(self):  # dE/dw = -(target(o1)-out(o1)) * out(o1)*(1-out(o1))*out(h1) , out(o1)*(1-out(o1)) = sigmoid_prime(out(o1))
 
         tmp_weights = []
         self.all_outputs = [self.training_data] + self.all_outputs
 
-
-
-        #do network output layer
-
-
-
+        # do network output layer
 
         delta = []
         deltah = []
 
         for i in range(self.layers[-1]):
-            delta.append(-(self.checking_data[i]-self.all_outputs[-1][i]) * sigmoid_prime(self.all_outputs[-1][i]))
-        
+            delta.append(-(self.checking_data[i] - self.all_outputs[-1][
+                         i]) * sigmoid_prime(self.all_outputs[-1][i]))
 
         neuron_count_previous = self.layers[-2]
 
-
-
-        for i in range(len(self.weights[-1])/2):
+        for i in range(len(self.weights[-1]) / 2):
             discard_delta = delta[i]
             for j in range(neuron_count_previous):
-                tmp_weights.append(self.weights[-1][2*i+j] - self.learning_rate*discard_delta*self.all_outputs[-2][j])
+                tmp_weights.append(
+                    self.weights[-1][2 * i + j] - self.learning_rate * discard_delta * self.all_outputs[-2][j])
 
+        # do the rest
 
-
-
-        #do the rest
-
-
-        #for l in range(2,-len(layers),1):
+        # for l in range(2,-len(layers),1):
 
         for i in range(self.layers[-2]):
 
-            delta_tmp = sum(delta[k]*self.weights[-1][k] for k in range(self.layers[-2]))
+            delta_tmp = sum(delta[k] * self.weights[-1][k]
+                            for k in range(self.layers[-2]))
 
-            deltah.append(delta_tmp*sigmoid_prime(self.all_outputs[-2][i])) #check sum
+            # check sum
+            deltah.append(delta_tmp * sigmoid_prime(self.all_outputs[-2][i]))
 
-
-        for i in range(len(self.weights[-2])/2):
+        for i in range(len(self.weights[-2]) / 2):
             discard_delta = deltah[i]
-            for j in range(neuron_count_previous): 
-                tmp_weights.append(self.weights[-2][2*i+j] - self.learning_rate*discard_delta*self.all_outputs[-3][j])
-
-
-
-
+            for j in range(neuron_count_previous):
+                tmp_weights.append(
+                    self.weights[-2][2 * i + j] - self.learning_rate * discard_delta * self.all_outputs[-3][j])
 
         chunksize = 4
-        self.weights = list(reversed([tmp_weights[i:i + chunksize] for i in xrange(0, len(tmp_weights), chunksize)]))
+        self.weights = list(reversed(
+            [tmp_weights[i:i + chunksize] for i in xrange(0, len(tmp_weights), chunksize)]))
 
-
-
-    
     def run(self):
         error = 0
 
-        for i in range(1, self.epochs+1): #, self.epochs
+        for i in range(1, self.epochs + 1):  # , self.epochs
             outputs = self.feedforward(self.training_data, 1)
-            #print(outputs)
-            error = sum(0.5*np.square(self.checking_data[i]-outputs[i]) for i in range(len(outputs)))
+            # print(outputs)
+            error = sum(
+                0.5 * np.square(self.checking_data[i] - outputs[i]) for i in range(len(outputs)))
 
             """
 
@@ -111,8 +100,8 @@ class network(object):
 
             self.backprop()
 
-            if i % (self.epochs/10) == 0:
-                print(('epoch: ' + str(i) + ', MSE: ' + str(error)) )
+            if i % (self.epochs / 10) == 0:
+                print(('epoch: ' + str(i) + ', MSE: ' + str(error)))
 
             """
 
@@ -126,9 +115,9 @@ class network(object):
 
             """
 
-            #print([self.training_data] + self.all_outputs)
+            # print([self.training_data] + self.all_outputs)
 
-            self.all_outputs = [] #reset outputs
+            self.all_outputs = []  # reset outputs
 
         print('')
         print('outputs at final epoch:')
