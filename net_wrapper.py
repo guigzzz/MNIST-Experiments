a24c2234f58b878c90b23fa49d3d4501024de391
Guillaume <rameg@neuf.fr>
2016-09-01 12:24:37 +0200

    weights and biases now auto initialise randomly

--- net_wrapper.py
+++ net_wrapper.py
@@ -9,18 +9,14 @@ checking_data = [0.01, 0.99]
 
 layers = [2, 2, 2]
 
-epochs = 100000
+epochs = 10000
 
-learning_rate = 0.5
-
-biases = [0.35, 0.6]
-
-weights = [[0.15, 0.2, 0.25, 0.3], [0.4, 0.45, 0.5, 0.55]]
+learning_rate = 60
 
 # initialise network
 
 net = network.network(training_data, checking_data, layers, epochs,
-                      learning_rate, biases, weights)
+                      learning_rate)
 
 # run network
 
--- network.py
+++ network.py
@@ -1,27 +1,44 @@
 import numpy as np
-#import random
+from random import uniform
 
 
 class network(object):
 
-    def __init__(self, training_data, checking_data, layers, epochs, learning_rate, biases, weights):
+    def __init__(self, training_data, checking_data, layers, epochs, learning_rate):
         self.training_data = training_data
         self.checking_data = checking_data
         self.layers = layers
         self.epochs = epochs
         self.learning_rate = learning_rate
-        self.biases = biases
-        self.weights = weights  # add random generation
+        # self.biases = biases
+        # self.weights = weights  # add random generation
 
-        # self.biases = [np.random.randn(y, 1) for y in layers[1:]]
-        # self.weights = [np.random.randn(y, x) for x, y in zip(layers[:-1], layers[1:])]
-        """
+        self.biases = [uniform(y, 1) for y in layers[1:]]
+
+        number_of_weights = []
+        for i in range(len(layers) - 1):
+            number_of_weights.append(layers[i] * layers[i + 1])
+
+        self.weights = []
+        for k in range(len(number_of_weights)):
+            self.weights.append([uniform(0, 1)
+                                 for i in range(number_of_weights[k])])
+
+        
         print('biases:')
         print(self.biases)
         print('weights')
         print(self.weights)
-        """
+        print('\n')
+        
+
         self.all_outputs = []
+
+    def div_to_chunks(self, tmp_weights):
+
+        chunksize = len(self.weights[0])
+        self.weights = list(reversed(
+            [tmp_weights[i:i + chunksize] for i in xrange(0, len(tmp_weights), chunksize)]))
 
     def feedforward(self, data, layer_count):
 
@@ -32,7 +49,7 @@ class network(object):
         if layer_count != len(self.layers):
             outputs = []
             for neuron in range(self.layers[layer_count]):
-                output = sigmoid(sum(data[i] * self.weights[(layer_count - 1)][2 * neuron + i]
+                output = sigmoid(sum(data[i] * self.weights[(layer_count - 1)][self.layers[layer_count - 1] * neuron + i]
                                      for i in range(self.layers[layer_count - 1])) + self.biases[layer_count - 1])
                 outputs.append(output)
 
@@ -55,33 +72,33 @@ class network(object):
             delta.append(-(self.checking_data[i] - self.all_outputs[-1][
                          i]) * sigmoid_prime(self.all_outputs[-1][i]))
 
-        for i in range(len(self.weights[-1]) / 2):
+        for i in range(self.layers[-1]):
             discard_delta = delta[i]
             for j in range(self.layers[-2]):
                 tmp_weights.append(
-                    self.weights[-1][2 * i + j] - self.learning_rate * discard_delta * self.all_outputs[-2][j])
+                    self.weights[-1][self.layers[-2] * i + j] - self.learning_rate * discard_delta * self.all_outputs[-2][j])
 
-        # do the rest
+        # do the other layers, going thru network backwards
 
         for l in range(2, len(self.layers)):
 
             for i in range(self.layers[-l]):
 
-                delta_tmp = sum(delta[k] * self.weights[-l + 1][k]
-                                for k in range(self.layers[-l]))
+                delta_tmp = sum(delta[k] * self.weights[-l + 1][k * self.layers[-l + 1] + i]
+                                for k in range(self.layers[-l + 1]))
 
-                # check sum
                 deltah.append(
                     delta_tmp * sigmoid_prime(self.all_outputs[-l][i]))
 
             for i in range(len(self.weights[-l]) / 2):
                 discard_delta = deltah[i]
-                for j in range(self.layers[-l]):
-                    tmp_weights.append(self.weights[-l][2 * i + j] - self.learning_rate * discard_delta * self.all_outputs[-l - 1][j])
+                for j in range(self.layers[-l - 1]):
+                    tmp_weights.append(
+                        self.weights[-l][self.layers[-l - 1] * i + j] - self.learning_rate * discard_delta * self.all_outputs[-l - 1][j])
 
-        chunksize = 4
-        self.weights = list(reversed(
-            [tmp_weights[i:i + chunksize] for i in xrange(0, len(tmp_weights), chunksize)]))  # concatenate into the format defined in net_wrapper
+            delta = deltah
+
+            self.div_to_chunks(tmp_weights)
 
     def run(self):
         error = 0
@@ -101,12 +118,6 @@ class network(object):
             # print([self.training_data] + self.all_outputs)
 
             self.all_outputs = []  # reset outputs
-        """
-        print('')
-        print('outputs at final epoch:')
-        print('')
-        print(outputs)
-        """
 
 
 def sigmoid(z):
