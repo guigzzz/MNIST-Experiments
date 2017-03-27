ff9fd624bdac545d6dcea0b46bb5672e43c7fc84
guigzzz <rameg@neuf.fr>
2017-03-27 22:39:06 +0200

    started crossval, nn works on iris dataset

--- net_wrapper.py
+++ net_wrapper.py
@@ -1,10 +1,10 @@
 from nn import network
 import numpy as np
 
-train_data = []
+'''train_data = []
 f = open('zip.train')
 for line in f:
-    curline = line.split(' ')[:-2]
+    curline = line.split(' ')[:-1]
     if (float(curline[0])==2.0) | (float(curline[0])==8.0):
         train_data.append([float(x) for x in curline])
 f.close()
@@ -16,13 +16,38 @@ for data in train_data:
 
 train_data = np.array(train_data)
 labels = train_data[:,0]
+train_data = train_data[:,1:]
+layers = [256]'''
+
+train_data = []
+classes = []
+f = open('iris.data')
+for line in f:
+    curline = line.split('\n')[0].split(',')
+    train_data.append([float(x) for x in curline[:-1]])
+    if float(curline[-1])==0.0:
+        classes.append([0,0,1])
+    elif float(curline[-1])==1.0:
+        classes.append([0,1,0])
+    else:
+         classes.append([1,0,0])
+f.close()
+layers = [10]
+
+'''train_data = np.array(train_data)
+labels = train_data[:,0]
 print labels
 train_data = train_data[:,1:]
-layers = [256,256,1]
+layers = [256,256,1]'''
 
-'''train_data = [[0,0],[0,1],[1,0],[1,1]]
-labels = [0,1,1,0]
-layers = [2,2,1]'''
+'''train_data = [[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]]
+labels = [0,0,0,0,0,0,0,1]
+layers = []'''
 
-net = network(train_data,labels,layers,0.7,1000)
-net.train()
+'''train_data = [[0],[1],[2],[3]]
+labels = [0,1,2,3]
+layers = [1,2,1]'''
+
+net = network(train_data,classes,layers,0.2,1000)
+#net.train()
+net.crossvalidate(10,True)
--- nn.py
+++ nn.py
@@ -4,20 +4,27 @@ class network(object):
 
     def __init__(self, train_data, labels, layers,learning_rate, epochs):
         self.train_data = train_data
+
+        input_size = len(train_data[0])
+        if type(labels[0])==list:
+            output_size=len(labels[0])
+        else:
+            output_size=1
+
         self.labels = labels
-        self.layers = layers
+        self.layers = [input_size] + layers + [output_size]
         self.epochs = epochs
         self.learning_rate = learning_rate
         self.num_layers = len(self.layers)
 
         self.weights = [
-                        np.random.rand(layers[k+1],layers[k]+1)
-                        for k in range(len(layers)-1)
+                        np.random.rand(self.layers[k+1],self.layers[k]+1)
+                        for k in range(len(self.layers)-1)
                        ]
 
-        self.neuron_values = [np.ones(n+1) for n in layers]
-        self.z = [np.ones(n+1) for n in layers]
-        self.delta = [np.ones(n) for n in layers[1:]]
+        self.neuron_values = [np.ones(n+1) for n in self.layers]
+        self.z = [np.ones(n+1) for n in self.layers]
+        self.delta = [np.ones(n) for n in self.layers[1:]]
 
         
     def sigmoid(self,z):
@@ -41,9 +48,8 @@ class network(object):
 
         #all other layers
         for i in range(2,self.num_layers):
-
             self.delta[-i] = \
-                self.sigmoid_prime(self.z[-i+1][1:]) * \
+                self.sigmoid_prime(self.z[-i][1:]) * \
                     np.dot(self.weights[-i+1][:,1:].T,self.delta[-i+1])
 
         #update weights
@@ -52,34 +58,59 @@ class network(object):
      
 
     def train(self):
-        predicted_labels = []
+        print 'training....'
         for i in range(self.epochs):
+
+            if i%(round(self.epochs/10))==0:
+                print("current epoch: " + str(i))
+
             for j in range(len(self.train_data)):
                 self.feedforward(self.train_data[j])
                 self.backpropagate(self.labels[j])
-
-                for k in range(1,len(self.neuron_values[-1])):
-                    if self.neuron_values[-1][k] > 0.9:
-                        self.neuron_values[-1][k] = 1
-                    elif self.neuron_values[-1][k] < 0.1:
-                        self.neuron_values[-1][k] = 0
-                        
-                predicted_labels.append(neuron_values[-1][1:])
-
-            print error(predicted_labels,self.labels)
             
     def error(self,predicted,ref):
-        return sum(int(pred==ref) for pred,re in zip(predicted,ref))/len(ref)
+        return sum(int(pred!=re) for pred,re in zip(predicted,ref))/len(ref)
 
     def MSE(self,predicted_labels,ref_labels):
         error = 0
         for pred,ref in zip(predicted_labels,ref_labels):
-            error += (pred-ref)**2
+            error += (pred[0]-ref)**2
         return error/len(predicted_labels)
 
     def predict(self,input):
-        self.feedforward(input)
-        print self.neuron_values[-1][1:]
+        output = []
+        for datapoint in input:
+            self.feedforward(datapoint)
+            #print self.neuron_values
+            
+            output.append(list(self.neuron_values[-1][1:]))
+        return output
 
-    def crossvalidate(self,folds):
-        pass
+    def roundclasses(self,input):
+        output = []
+        for i in range(len(input)):
+            output.append([int(round(x)) for x in input[i]])
+        return output
+        
+
+    def crossvalidate(self,folds,randomise=False):
+        if randomise:
+            from random import shuffle
+            items = list(self.train_data)
+            shuffle(self.train_data)
+
+        slices = [self.train_data[i::folds] for i in range(folds)]
+
+        for sub in slices:
+            self.train_data = []
+            self.train_data = [self.train_data + sl for sl in slices if sl!=sub]
+            validation = sub
+
+            print "train data"
+            print self.train_data
+            print "val"
+            print validation
+            self.train()
+            predictions = self.predict(validation)
+            predictions = self.roundclasses(predictions)
+            print "validation error: " + str(self.error(predictions,self.labels))
