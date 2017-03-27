ce0c1b99bc5a24c51ec67847a37904a1c2acd46d
guigzzz <rameg@neuf.fr>
2017-03-27 16:06:05 +0200

    nearly working

--- nn.py
+++ nn.py
@@ -10,6 +10,7 @@ class network(object):
         self.layers = layers
         self.epochs = epochs
         self.learning_rate = learning_rate
+        self.num_layers = len(self.layers)
 
         self.weights = [
                         np.random.rand(layers[k+1],layers[k]+1)
@@ -17,54 +18,72 @@ class network(object):
                        ]
 
         self.neuron_values = [np.ones(n+1) for n in layers]
-        self.befactiv = [np.ones(n+1) for n in layers]
-        self.delta = [np.ones(n+1) for n in layers]
+        self.z = [np.ones(n+1) for n in layers]
+        self.delta = [np.ones(n) for n in layers[1:]]
         
-        print('weights')
+        '''print('weights')
         print(self.weights)
         print('neuron values')
         print(self.neuron_values)
-        print('\n')
+        print "layers"
+        print self.layers
+        print('\n')'''
 
         
     def sigmoid(self,z):
         return 1.0 / (1.0 + np.exp(-z))
 
     def sigmoid_prime(self,z):
-        return z * (1 - z)
-
+        return self.sigmoid(z) * (1 - self.sigmoid(z))
     
     def feedforward(self, input):
-
         self.neuron_values[0] = np.array([1] + input)
 
-        for i in range(1,len(self.layers)): #layer
-            self.befactiv[i][1:] = np.dot(self.weights[i-1],self.neuron_values[i-1])
-            self.neuron_values[i][1:] = self.sigmoid(self.befactiv[i][1:])
+        for i in range(1,self.num_layers): #layer
+            self.z[i][1:] = np.dot(self.weights[i-1],self.neuron_values[i-1])
+            self.neuron_values[i][1:] = self.sigmoid(self.z[i][1:])
         
 
     def backpropagate(self,label):
          #output layer
-        self.delta[len(self.layers)-1][1:] = \
-            (self.neuron_values[ len(self.layers)-1 ][1:] - label) \
-                * self.sigmoid_prime(self.neuron_values[ len(self.layers)-1 ][1:])
-        #print "deltas"
-        #print self.delta
-        #print "neuron vals"
-        #print self.neuron_values
+        self.delta[ -1 ] = (self.neuron_values[ -1 ][1:] - label) * \
+                                self.sigmoid_prime(self.z[ -1 ][1:])
+            
+        '''print "deltas"
+        print self.delta'''
 
         #all other layers
-        for i in range(len(self.layers)-2,-1,-1):
-            self.delta[i] = self.sigmoid_prime(self.befactiv[i]) * np.dot(self.weights[i].T,self.delta[i+1][1:])
+        for i in range(2,self.num_layers):
+            '''print "cur weights"
+            print self.weights[-i+1][:,1:].T
+            print"cur delta"
+            print self.delta[-i+1]
+            print "cur z"
+            print self.z[-i][1:]'''
 
-        #print self.weights
-        #print self.neuron_values
-        #print self.delta
+            self.delta[-i] = \
+                self.sigmoid_prime(self.z[-i+1][1:]) * \
+                    np.dot(self.weights[-i+1][:,1:].T,self.delta[-i+1])
 
+        '''print '\n'
+        print "weight update"
+        print "deltas"
+        print self.delta
+        print "neuron vals"
+        print self.neuron_values'''
         #update weights
-        for i in range(len(self.layers)-1):
-            self.weights[i] -= self.learning_rate * self.neuron_values[i] * self.delta[i]
-        
+        for i in range(0,self.num_layers-1):
+            '''print"neuron vals"
+            print np.matrix(self.neuron_values[i]).T
+            print "cur deltas"
+            print np.matrix(self.delta[i])
+            print "cur weights"
+            print self.weights[i]
+            print "res"
+            print np.dot(np.matrix(self.delta[i]).T,np.matrix(self.neuron_values[i]))''' 
+
+            self.weights[i] -= self.learning_rate *  np.dot(np.matrix(self.delta[i]).T,np.matrix(self.neuron_values[i])) 
+     
 
     def train(self):
         for i in range(self.epochs):
@@ -72,8 +91,9 @@ class network(object):
                 self.feedforward(self.train_data[j])
                 self.backpropagate(self.labels[j])
 
-    def predict(self):
-        pass
+    def predict(self,input):
+        self.feedforward(input)
+        print self.neuron_values[-1][1:]
 
     def crossvalidate(self,folds):
         pass
@@ -82,13 +102,42 @@ class network(object):
 
 
 
+train_data = []
+f = open('zip.train')
+for line in f:
+    curline = line.split(' ')[:-2]
+    if (float(curline[0])==2.0) | (float(curline[0])==8.0):
+        train_data.append([float(x) for x in curline])
+f.close()
+for data in train_data:
+    if data[0]==2:
+        data[0]=0
+    elif data[0]==8:
+        data[0]=1
 
+train_data = np.array(train_data)
+labels = train_data[:,0]
+print labels
+train_data = train_data[:,1:]
+layers = [256,256,1]
 
-train_data = [[0,0],[0,1],[1,0],[1,1]]
-labels = [-1,-1,-1,1]
-layers = [3,3,1]
+'''train_data = [[0,0],[0,1],[1,0],[1,1]]
+labels = [0,1,1,0]
+layers = [2,2,1]'''
+
 net = network(train_data,labels,layers,0.7,1000)
 net.train()
 #net.feedforward(train_data[0])
 #net.backpropagate(labels[0])
+net.predict(train_data[0])
+print labels[0]
+net.predict(train_data[1])
+print labels[1]
+net.predict(train_data[2])
+print labels[2]
+net.predict(train_data[3])
+print labels[3]
+'''print "neuron vals"
 print net.neuron_values
+print "delta"
+print net.delta'''
