ebbc5a8ecd91ca6cb60876c0d3ae0d918992bb2e
guigzzz <gr1714@ic.ac.uk>
2017-03-31 18:08:38 +0200

    added ReLU activation function, softmax and Xentropy

--- net_wrapper.py
+++ net_wrapper.py
@@ -8,19 +8,22 @@ f.close()
 
 classes = [mapto1hot(x,10) for x in data[:,0]]
 train_data = data[:,1:]
-layers = [32]'''
+layers = [30]'''
 
-'''train_data,classes = import_mnist('mnist_data/train.idx3-ubyte','mnist_data/train_labels.idx1-ubyte')
+train_data,classes = import_mnist('mnist_data/train.idx3-ubyte','mnist_data/train_labels.idx1-ubyte')
 classes = [mapto1hot(x,10) for x in classes]
-train_data = list(np.array(train_data) / 255)
-layers = [100]'''
+train_data = list(np.array(train_data) / 255.0)
+layers = [30]
 
-f = open('iris.data')
+'''f = open('iris.data')
 data = np.loadtxt(f,delimiter=',')
 f.close()
 classes = [mapto1hot(x,3) for x in data[:,-1]]
 train_data = data[:,:-1]
-layers = [10]
+layers = [10]'''
 
-net = network(train_data,classes,layers,0.2,100)
+net = network(train_data,classes,layers,0.0005,20,\
+    hidden_activation_function = "ReLU",\
+    output_activation_function = "SoftMax")
+
 net.crossvalidate(10,randomise = True)
--- nn.py
+++ nn.py
@@ -2,7 +2,10 @@ import numpy as np
 
 class network(object):
 
-    def __init__(self, train_data, labels, layers, learning_rate, epochs):
+    def __init__(self, train_data, labels, layers, learning_rate,\
+         epochs,hidden_activation_function = None, \
+         output_activation_function = None):
+
         self.train_data = train_data
 
         input_size = len(train_data[0])
@@ -24,18 +27,42 @@ class network(object):
         self.z = [np.ones(n) for n in self.layers]
         self.delta = [np.ones(n) for n in self.layers[1:]]
 
+        if hidden_activation_function == None:
+            print "---- Using sigmoid activation for hidden layers ----"
+            self.activation_function = self.sigmoid
+            self.activation_function_prime = self.sigmoid_prime
+
+        elif hidden_activation_function == "ReLU":
+            print "---- Using Rectified Linear Unit activation for hidden layers ----"
+            self.activation_function = self.ReLU
+            self.activation_function_prime = self.ReLU_prime
+
+        elif hidden_activation_function == "LReLU":
+            print "---- Using Leaky Rectified Linear Unit activation for hidden layers ----"
+            self.activation_function = self.LReLU
+            self.activation_function_prime = self.LReLU_prime
+            self.leakage_parameter = 0.01
+
+        #typical linear output with MSE cost
+        if output_activation_function == None:
+            print "---- Using linear output with MSE cost ----"
+            self.output_activation = (lambda x: x) #linear output function
+            self.output_activation_prime = (lambda x: [1] * len(x)) #derivative is 1
+            self.delta_output = self.output_delta_MSE
+        
+        #softmax output with XEntropy cost
+        elif output_activation_function == "SoftMax":
+            print "---- Using softmax output with XEntropy cost ----"
+            self.output_activation = self.softmax
+            self.cost_function_prime = self.cross_entropy_for_softmax_prime
+            self.delta_output = self.output_delta_sm_xent
+
+
     def init_weights(self):
         self.weights = [
                         np.random.rand(self.layers[k+1],self.layers[k]+1)
                         for k in range(len(self.layers)-1)
                        ]
-
-    def tanh(self,z):
-        return self.sigmoid(2*z)-self.sigmoid(-2*z)
-    
-    def tanh_prime(self,z):
-        sigres = self.sigmoid(z)
-        return 1 - sigres*sigres
 
     def sigmoid(self,z):
         return 1.0 / (1.0 + np.exp(-z))
@@ -44,32 +71,63 @@ class network(object):
         sigres = self.sigmoid(z)
         return sigres * (1 - sigres)
 
-    def cross_entropy_cost_prime(z,label):
-        return (z - label)/(z * (1 - z))
+    def ReLU(self,z):
+        return np.maximum(0,z)
 
-    def MSE_cost_prime(z,label):
+    def ReLU_prime(self,z):
+        tmp = np.zeros(len(z))
+        tmp[np.greater(z,0)] = 1
+        return tmp
+
+    def LReLU(self,z):
+        tmp = np.zeros(len(z))
+        indices = z > 0
+        tmp[indices] = z[indices]
+        tmp[~indices] = self.leakage_parameter * z[~indices]
+        return tmp
+
+    def LReLU_prime(self,z):
+        tmp = np.zeros(len(z))
+        indices = z > 0
+        tmp[indices] = 1
+        tmp[~indices] = self.leakage_parameter
+        return tmp
+
+    def cross_entropy_for_softmax_prime(self,z,labels):
+        return z - labels
+
+    def softmax(self,z):
+        stable = np.exp(z - np.max(z)) #to avoid NaNs
+        return stable / np.sum(stable)
+
+    def MSE_cost_prime(self,z,label):
         return z - label
-    
-    def feedforward(self, input):
 
+    def output_delta_MSE(self,neuron_vals,targets,der_output):
+        return self.MSE_cost_prime(neuron_vals,targets) * self.output_activation_prime(der_output)
+
+    def output_delta_sm_xent(self,neuron_vals,targets,der_output):
+        return self.cross_entropy_for_softmax_prime(neuron_vals,targets)
+
+    def feedforward(self, input):
         self.neuron_values[0][1:] = input
 
-        for i in range(1,self.num_layers): #layer
-
+        for i in range(1,self.num_layers-1):
             self.z[i] = np.dot(self.weights[i-1],self.neuron_values[i-1])
-            self.neuron_values[i][1:] = self.sigmoid(self.z[i])
+            self.neuron_values[i][1:] = self.activation_function(self.z[i])
         
+        self.z[-1] = np.dot(self.weights[-1],self.neuron_values[-2])
+        self.neuron_values[-1][1:] = self.output_activation(self.z[-1])
 
-    def backpropagate(self,label,Output_activation = MSE_cost_prime):
+    def backpropagate(self,label):
+        
         #output layer
-        self.delta[ -1 ] = Output_activation(self.neuron_values[ -1 ][1:],label) * \
-                                self.sigmoid_prime(self.z[ -1 ])
-
+        self.delta[ -1 ] = self.delta_output(self.neuron_values[ -1 ][1:],label,self.z[ -1 ])
+        
         #all other layers
         for i in range(2,self.num_layers):
-
             self.delta[-i] = \
-                self.sigmoid_prime(self.z[-i]) * \
+                self.activation_function_prime(self.z[-i]) * \
                     np.dot(self.weights[-i+1][:,1:].T,self.delta[-i+1])
 
         #update weights
@@ -78,24 +136,27 @@ class network(object):
      
 
     def train(self):
-        '''last_err = 1
+
+        last_err = 1
         self.learning_rate = self.cv_starting_rate
-        old_weights = []'''
+        old_weights = []
         print 'training....'
         for i in range(self.epochs):
             train_error = self.testmodel(self.train_data,self.labels,True)
-            '''if train_error <= last_err:
+            if train_error <= last_err:
                 self.learning_rate *= 1.1
                 old_weights = list(self.weights)
             else:
                 self.learning_rate *= 0.5
                 self.weights = list(old_weights)
-            last_err = train_error'''
+            last_err = train_error
 
-            if i%(round(self.epochs/10))==0:
+            print("current epoch: " + str(i) + "/" + str(self.epochs) + " train error: " + str(train_error) + " learning rate: " + str(self.learning_rate))
+
+            '''if i%(round(self.epochs/10))==0:
                 
                 print("current epoch: " + str(i) + "/" + str(self.epochs) + " train error: " + str(train_error) + " learning rate: " + str(self.learning_rate))
-
+            '''
             for j in range(len(self.train_data)):
                 
                 self.feedforward(self.train_data[j])
