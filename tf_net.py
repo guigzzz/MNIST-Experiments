6b5b32adf0ffdda85c093d35ec34cfce56b266f5
Guillaume Ramé <gr1714@ic.ac.uk>
2017-07-01 12:15:05 +0100

    added fc net class and tensorboard usage

--- tf_net.py
+++ tf_net.py
@@ -3,30 +3,31 @@ from tensorflow.examples.tutorials.mnist import input_data
 import sys
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-train_data = mnist.train.images
-train_labels = mnist.train.labels
-test_data = mnist.test.images
-test_labels = mnist.test.labels
 
-def fc_layer(x, in_size, out_size, activation = 'relu'):
-    W = tf.Variable(tf.random_normal([in_size,out_size], stddev = 0.1))
-    b = tf.Variable(tf.constant(.05, shape=[out_size]))
-    y = tf.matmul(x, W) + b
-    if activation == 'relu':
-        return tf.nn.relu(y)
-    elif activation == 'logit':
-        return y
-    else:
-        print('unknown activation function')
-        sys.exit()
+class fcnet():
+    def __init__(self, layer_sizes, activation = tf.nn.relu):
+        self.activation = activation
+        self.layer_sizes = layer_sizes
+        self.num_layers = len(self.layer_sizes)
+
+    def build(self, x):
+        for i in range(self.num_layers - 2):
+            x = self.fc_layer(x, self.layer_sizes[i], self.layer_sizes[i+1], activation = self.activation)
+
+        return self.fc_layer(x, self.layer_sizes[-2], self.layer_sizes[-1], tf.identity)
+    
+    def fc_layer(self, x, in_size, out_size, activation):
+        W = tf.Variable(tf.random_normal([in_size,out_size], stddev = 0.1))
+        b = tf.Variable(tf.constant(.05, shape=[out_size]))
+        y = tf.matmul(x, W) + b
+        return activation(y)
+
 
 x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
-fc1 = fc_layer(x, 784, 500)
-fc2 = fc_layer(fc1, 500, 500)
-fc3 = fc_layer(fc2, 500, 500)
-y = fc_layer(fc3, 500, 10, activation = 'logit')
+net = fcnet([784, 500, 10])
+y = net.build(x)
 
 cross_entropy = tf.reduce_mean(
     tf.nn.softmax_cross_entropy_with_logits(labels = y_, logits = y))
@@ -36,23 +37,21 @@ train_step = tf.train.AdamOptimizer().minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+tf.summary.scalar("loss", cross_entropy)
+tf.summary.scalar("train set accuracy", accuracy)
+merged = tf.summary.merge_all()
+
 init = tf.global_variables_initializer()
+logs_path = 'tf_logs'
 
 with tf.Session() as sess:
     sess.run(init)
+    summary_writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph())
     for i in range(5000):
         batch = mnist.train.next_batch(100)
-        train_step.run(feed_dict={x: batch[0], y_: batch[1]})
 
+        _, summary = sess.run([train_step, merged], feed_dict={x: batch[0], y_: batch[1]})
         if not i % 100:
-            print(accuracy.eval(feed_dict={x: mnist.train.images, y_: mnist.train.labels}))
+            summary_writer.add_summary(summary, i)
 
-
-    print('')
-    print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
-
-    
-
-    
-
-    
+    print('test accuracy: {}'.format(sess.run(accuracy, feed_dict = {x: mnist.test.images, y_: mnist.test.labels})))
